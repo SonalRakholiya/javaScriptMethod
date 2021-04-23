30128e69cb46ec0614bc9ade06f0455886abc691
jaimishakoldiya <jemishakoladiya2001@gmail.com>
2021-04-23 18:31:27 +0530

    changes

--- Js/ES6Begin.js
+++ Js/ES6Begin.js
@@ -1,5 +1,8 @@
 //********** Let And Const ********** //
 
+
+
+
 const name = 'sonal';
 // // var keyword
 // function checkvar()
@@ -131,7 +134,7 @@ function Sum(...inputs) {
 }
 Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-function FuncRest(a,b,...c){
+function FuncRest(a, b, ...c) {
     console.log(`${a} ${b}`);
     console.log(c);
     console.log(c[0]);
@@ -139,21 +142,21 @@ function FuncRest(a,b,...c){
     console.log(c.indexOf("Edan"));
 
 }
-FuncRest("Ronaldo","naymar","Pele","Messi","Edan");
+FuncRest("Ronaldo", "naymar", "Pele", "Messi", "Edan");
 
 //********** Spread Parameters ********** //
 
-let arr1 = [1,2,3]
-let arr2 = [4,5,6]
-let arr3 = [7,8,9]
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
+let arr3 = [7, 8, 9]
 
 //arr1=arr1.concat(arr2);
-arr1=[...arr1,...arr2,...arr3];
+arr1 = [...arr1, ...arr2, ...arr3];
 
 console.log(arr1);
 
 
-let arrc = [...arr1,4,5,...arr3,10];
+let arrc = [...arr1, 4, 5, ...arr3, 10];
 console.log(arrc);
 console.log(arr1);
 
@@ -161,3 +164,113 @@ console.log(arr1);
 //‪D:\Sonal\ScrambleWord\ScrambleGame.html
 
 // Math Object in JavaScript | Math.sign(), Math.trunc() 
+
+
+
+// CallBack
+function myDisplayer(some) {
+    //document.getElementById("demo").innerHTML=some;    
+    console.log(some);
+}
+
+function myCalculater(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+}
+myCalculater(5, 2, myDisplayer);
+
+// Promise
+
+// let myPromise = new Promise(function (myResolve, myReject) {
+//     let x = 1;
+//     if (x == 0) { myResolve("OK"); }
+//     else { myReject("Error"); }
+
+// });
+// myPromise.then(
+//     function (value) { myDisplayer(value); },
+//     function (error) { myDisplayer(error); }
+// );
+
+// **********  Async Await in JavaScript  ********** // 
+
+const pobj1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let RollNo = [1, 2, 3, 4, 5];
+        resolve(RollNo);
+    }, 2000);
+});
+
+const getBiodData = (indexdata) => {
+    return new Promise((resolve, reject) => {
+        setTimeout((indexdata) => {
+            let biodata = {
+                name: "Sonal",
+                age: 25
+            }
+            resolve(`My Name is ${name} my age is ${age}`);
+        }, 2000, indexdata)
+    });
+}
+
+async function getData() {
+    try {
+        const Rolldata = await pobj1;
+        console.log(Rolldata);
+
+        const MyInfo = await getBiodData();
+        console.log(MyInfo);
+
+        return MyInfo;
+    }
+    catch (error) {
+        console.log(`Thee Error is : ${error}`)
+    }
+}
+
+// const GetNames = getData().then((myname) => {
+//    console.log(myname);
+// });
+
+
+// **********  Make Multiple AJAX Call using Function with Fetch API in Promises   ********** // 
+
+function getCovidData(index) {
+    fetch(`https://api.covid19api.com/summary`).then((apidata) => {
+        console.log(apidata);
+        return apidata.json();
+    }).then((actualdata) => {
+        // console.log(actualdata.Countries);
+        const Mydata = actualdata.Countries[`${index}`];
+
+        console.log(`The Country Name is ${Mydata.Country}.
+The Total Confirmed cases here is ${Mydata.TotalConfirmed}.
+The Total Deaths cases here is ${Mydata.TotalDeaths}.
+The Total Recovered cases here is ${Mydata.TotalRecovered}.`);
+    }).catch((error) => {
+        console.log(`The Error is : ${error}`);
+    });
+
+
+}
+getCovidData(76);
+
+// **********  Error Handling in Async Await Fetch   ********** // 
+
+async function GetCovidDataAsync(index) {
+    try { 
+const jsondata= await fetch(`https://api.covid19api.com/summary`);
+//const Mydata = actualdata.Countries[`${index}`];
+const Mydata = await jsondata.json();
+const Country_Info=jsondata.Countries[76];
+console.log(`The Country Name is ${Country_Info.Country}.
+The Total Confirmed cases here is ${Country_Info.TotalConfirmed}.
+The Total Deaths cases here is ${Country_Info.TotalDeaths}.
+The Total Recovered cases here is ${Country_Info.TotalRecovered}.`);
+
+
+    } catch (error){ 
+        console.log(`The ERror is ${error}` );
+    }
+
+}
